e59f08c12d23dd3a6529841e21f84c722dbd280c
Justin Bronder <jsbronder@cold-front.org>
2021-03-11 21:07:40 -0500

    setup: add dbus-python to list of dependencies (#13)
    Fixes #12

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     include_package_data=True,
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
     install_requires=[
+        'dbus-python',
         'setuptools',
         'future',
     ],
